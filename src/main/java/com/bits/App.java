464919b438a858782ab90dd9c475dcbdee470478
Durgadevi S <2022wb86365@wilp.bits-pilani.ac.in>
2026-09-22 19:52:57 +0530

    Modified source cod* for CI testing

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Durgadevi-2022wb86365 in DevOps Lab!" );
     }
 }
